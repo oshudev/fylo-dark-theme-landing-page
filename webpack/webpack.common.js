16ebe4a79c256d450d6ef4fa49e35618a6b1a834
oshudev <99813212+eurus1108@users.noreply.github.com>
2024-07-30 20:15:13 +0800

    refactor: change webpack config

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,16 +1,6 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const path = require("path");
-
 module.exports = {
   entry: {
     app: "./src/app.js",
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "../dist"),
-    clean: true,
   },
   module: {
     rules: [
@@ -19,27 +9,12 @@ module.exports = {
         loader: "html-loader",
       },
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
         test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
         type: "asset/resource",
         generator: {
-          filename: "assets/[name].[hash][ext]",
+          filename: "assets/[name].[hash].[ext]",
         },
       },
     ],
   },
-  optimization: {
-    minimize: true,
-    minimizer: ["...", new CssMinimizerPlugin()],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.resolve(__dirname, "../src/index.html"),
-    }),
-    new MiniCssExtractPlugin(),
-  ],
 };
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,9 +1,14 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+  },
   devtool: "inline-source-map",
   devServer: {
     static: path.resolve(__dirname, "dist"),
@@ -12,5 +17,18 @@ module.exports = merge(common, {
     compress: true,
     watchFiles: ["src/**/*.html"],
     historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../src/index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 });
